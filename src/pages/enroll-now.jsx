38f528f9e6ecc691c247a7765c870d92d02987c7
Ategekap3 <piersiepertegiealaari@gmail.com>
2025-10-01 11:48:42 +0400

    changed components case

--- src/pages/enroll-now.jsx
+++ src/pages/enroll-now.jsx
@@ -1,11 +1,14 @@
 // src/pages/enroll-now.jsx
 import React from 'react';
-import Navbar from '../Components/Navbar';
+import Navbar from '../components/Navbar';
 import EnrollmentForm from '../components/EnrollmentForm';
 
 function Enroll({ addAdmission }) {
   return (
+    
+  
     <div>
+    
       <EnrollmentForm addAdmission={addAdmission} />
     </div>
   );
